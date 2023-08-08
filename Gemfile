24aa491e3eba27050c8aa93d62f18dcf7605c6da
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2023-08-08 12:16:57 +0600

    Remove hardcoded barbers list

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
 
 gem 'sinatra', '~> 3.0', '>= 3.0.6'
 gem 'sinatra-reloader', '~> 1.0'
--- app.rb
+++ app.rb
@@ -1,26 +1,29 @@
-#encoding: utf-8
+# frozen_string_literal: true
+
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
-def is_barber_exists? db, name
-  db.execute('select * from Barbers where name=?', [name]).length > 0
+def is_barber_exists?(db, name)
+  db.execute('select * from Barbers where name=?', [name]).length.positive?
 end
 
-def seed_db db, barbers
+def seed_db(db, barbers)
   barbers.each do |barber|
-    if !is_barber_exists? db, barber
-      db.execute 'insert into Barbers (name) values (?)', [barber]
-    end
+    db.execute 'insert into Barbers (name) values (?)', [barber] unless is_barber_exists? db, barber
   end
 end
 
-configure do
-
+before do
   db = SQLite3::Database.new 'barbershop.db'
-  db.execute 'CREATE TABLE IF NOT EXISTS 
-    "Users" 
+  @barbers = db.execute 'select * from Barbers'
+end
+
+configure do
+  db = SQLite3::Database.new 'barbershop.db'
+  db.execute 'CREATE TABLE IF NOT EXISTS
+    "Users"
       (
         "id" INTEGER PRIMARY KEY AUTOINCREMENT,
         "username" TEXT,
@@ -30,37 +33,36 @@ configure do
         "color" TEXT
       )'
 
-   db.execute 'CREATE TABLE IF NOT EXISTS 
-    "Barbers" 
+  db.execute 'CREATE TABLE IF NOT EXISTS
+    "Barbers"
       (
         "id" INTEGER PRIMARY KEY AUTOINCREMENT,
         "name" TEXT
       )'
 
-    seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Mike Ehrmantraut']
+  seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Mike Ehrmantraut']
 end
 
 get '/' do
-	erb :welcome		
+  erb :welcome
 end
 get '/admin' do
   erb :admin
 end
 
 post '/admin' do
-   @login = params['login']
+  @login = params['login']
   @password = params['pass']
 
   if @login == 'admin' && @password == 'secret'
     db = SQLite3::Database.new 'barbershop.db'
     @message = db.execute 'select * from Users order by id desc'
     @title = 'Users in DATABASE'
-    erb :message
   else
-    @title = "Access denied"
-    @message = ["Try", "later"]
-    erb :message
+    @title = 'Access denied'
+    @message = %w[Try later]
   end
+  erb :message
 end
 
 get '/about' do
@@ -68,25 +70,26 @@ get '/about' do
 end
 
 get '/visit' do
+  @message = @barbers
   erb :visit
 end
 
-#Отправить данные
+# Отправить данные
 post '/visit' do
   @name = params[:name]
   @phone_number = params[:phone_number]
   @date_time = params[:date_time]
   @barber = params[:barber]
   @color = params[:color]
+  @message = @barbers
 
   db = SQLite3::Database.new 'barbershop.db'
   db.execute 'insert into Users (username, phone, date_stamp, barber, color)
     values (?, ?, ?, ?, ?)', [@name, @phone_number, @date_time, @barber, @color]
-  
+
   erb :visit
 end
 
 get '/contacts' do
   erb :contacts
 end
-
