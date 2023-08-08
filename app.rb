22315d8fe008d574744b79155d8b0e28c270c8af
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2023-08-08 11:34:07 +0600

    Add table Barbers to db

--- app.rb
+++ app.rb
@@ -4,6 +4,17 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def is_barber_exists? db, name
+  db.execute('select * from Barbers where name=?', [name]).length > 0
+end
+
+def seed_db db, barbers
+  barbers.each do |barber|
+    if !is_barber_exists? db, barber
+      db.execute 'insert into Barbers (name) values (?)', [barber]
+    end
+  end
+end
 
 configure do
 
@@ -18,6 +29,15 @@ configure do
         "barber" TEXT,
         "color" TEXT
       )'
+
+   db.execute 'CREATE TABLE IF NOT EXISTS 
+    "Barbers" 
+      (
+        "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+        "name" TEXT
+      )'
+
+    seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Mike Ehrmantraut']
 end
 
 get '/' do
